24460dbd31ade01fb17dee0c853026e712c8d267
Ritesh Sodwani <riteshsodwani@zohomail.in>
2026-05-30 19:48:08 +0530

    First Commit Books Project.

--- books.py
+++ books.py
@@ -15,9 +15,25 @@ BOOKS=[
 async def read_all_books():
     return BOOKS
 
+@app.get("/books/mybook")
+async def read_my_books():
+    return BOOKS
+
 
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold()==book_title.casefold():
             return book
+
+
+
+
+
+
+
+
+
+
+
+
