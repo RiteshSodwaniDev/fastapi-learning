8a6259053f37b44937bd19fe2b45803458ccfa03
Ritesh Sodwani <riteshsodwani.db@gmail.com>
2026-06-27 21:08:24 +0530

    Table CHanges Pushed for Postgresql Database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-SQLALCHEMY_DATABASE_URL='sqlite:///./todosappdb'
+#SQLALCHEMY_DATABASE_URL='postgresql://postgres:admin@localhost:5432/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL='mysql+pymysql://root:admin@127.0.0.1:3306/todoapplicationdatabase'
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
