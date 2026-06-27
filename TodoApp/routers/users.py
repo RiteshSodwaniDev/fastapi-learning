5ba17d5075d6f77e64b7e339826d597e9dcb9691
Ritesh Sodwani <riteshsodwani.db@gmail.com>
2026-06-27 23:23:42 +0530

    Alembic Changes

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -49,6 +49,16 @@ async def change_password(user:user_dependency,db:db_dependency,user_verificatio
     db.add(user_model)
     db.commit()
 
+@router.put("/phonenumber/{phone_number}",status_code=status.HTTP_204_NO_CONTENT)
+async def change_mobile_number(user:user_dependency,db:db_dependency,phone_number:str):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="User not found")
+    user_model=db.query(Users).filter(Users.id==user.get("id")).first()
+    user_model.phone_number=phone_number
+    db.add(user_model)
+    db.commit()
+
+
 
 
 
