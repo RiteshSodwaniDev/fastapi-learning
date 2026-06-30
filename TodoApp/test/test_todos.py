026788eade85f8264940f1483e533968d728195b
Ritesh Sodwani <riteshsodwani.db@gmail.com>
2026-07-01 01:56:50 +0530

    Pytest Changes

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -61,3 +61,36 @@ def test_read_all_authenticated(test_todo):
     assert response.status_code==status.HTTP_200_OK
     print(response.json())
     assert response.json()==[{'complete':False,'title':'learn to code','description':'Need to learn everyday','id':1,'priority':5,'owner_id':1}]
+
+
+def test_read_one_authenticated(test_todo):
+    response=client.get("/todo/1")
+    assert response.status_code==status.HTTP_200_OK
+    print(response.json())
+    assert response.json()=={'complete':False,'title':'learn to code','description':'Need to learn everyday','id':1,'priority':5,'owner_id':1}
+
+def test_read_one_authenticated_not_found():
+    response=client.get("/todo/99")
+    assert response.status_code==status.HTTP_404_NOT_FOUND
+    assert response.json()=={'detail':'Todo Not Found'}
+
+
+
+def test_create_todo(test_todo):
+    request_data={
+        'title':'New Todo',
+        'description':'New todo Description',
+        'priority':5,
+        'complete':False
+    }
+    response=client.post('/todo',json=request_data)
+    assert response.status_code==201
+
+    db=TestingSessionLocal()
+    model=db.query(Todos).filter(Todos.id==2).first()
+    assert model.title==request_data.get('title')
+    assert model.description==request_data.get('description')
+    assert model.priority==request_data.get('priority')
+    assert model.complete==request_data.get('complete')
+
+
